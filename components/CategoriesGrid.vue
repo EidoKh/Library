d2d870bbbd94e8849a0a1fb37c8f597c8b060b15
Edo Khudeeda <eido.khudyda@gmail.com>
2022-01-31 16:49:51 +0300

    new updates on categories

---
+++ components/CategoriesGrid.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div>
+      <h1 class="text-center text-5xl text-gray-600 my-4">
+        Best Categories {{ categories }}
+      </h1>
+    </div>
+
+    <div class="grid grid-cols-3 gap-4">
+      <div v-for="category in categories" :key="category">
+        <div
+          class="
+            w-full
+            h-64
+            md:mx-4
+            rounded-md
+            overflow-hidden
+            bg-cover bg-center
+          "
+          style="
+            background-image: url('https://images.unsplash.com/photo-1547949003-9792a18a2601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80');
+          "
+        >
+          <div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
+            <div class="px-10 max-w-xl">
+              <h2 class="text-2xl text-white font-semibold">
+                {{ category.category_name }}
+              </h2>
+              <p class="mt-2 text-gray-400">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Tempore facere provident molestias ipsam sint voluptatum
+                pariatur.
+              </p>
+              <button
+                class="
+                  flex
+                  items-center
+                  mt-4
+                  text-white text-sm
+                  uppercase
+                  font-medium
+                  rounded
+                  hover:underline
+                  focus:outline-none
+                "
+              >
+                <span>Shop Now</span>
+                <svg
+                  class="h-5 w-5 mx-2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const response = await $fetch("http://127.0.0.1:8000/api/all-categories");
+const categories = response.data;
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,7 @@
 <template>
   <hero />
-  <div class="container mx-auto bg-gray-50 p-8">
-    <div class="container mx-auto pt-8">
-      <h1 class="font-bold text-3xl">Blog Posts</h1>
-      {{ posts }}
-      <ul class="w-fit mt-8">
-        <li
-          v-for="post in posts"
-          :key="post.id"
-          class="
-            p-2
-            border-b-2 border-indigo-400
-            hover:bg-indigo-400 hover:text-white hover:cursor-pointer
-          "
-        >
-          <nuxt-link :to="'/posts/' + post.id">{{ post.title }}</nuxt-link>
-        </li>
-      </ul>
-    </div>
+  <div class="container mx-auto px-6 mt-12">
+    <categories-grid />
   </div>
 </template>
-<script setup>
-const posts = await $fetch("http://127.0.0.1:8000/api/categories");
-// const mountains = await $axios.$get(`/mountains`);
-</script>
+
