a2f69e335534998265ab614384d33acdad9f7615
Edo Khudeeda <eido.khudyda@gmail.com>
2022-02-01 09:04:08 +0300

    hfhff

--- components/CategoriesGrid.vue
+++ components/CategoriesGrid.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <div>
-      <h1 class="text-center text-5xl text-gray-600 my-4">
-        Best Categories {{ categories }}
-      </h1>
+      <h1 class="text-center text-5xl text-gray-600 my-4">Best Categories</h1>
     </div>
 
-    <div class="grid grid-cols-3 gap-4">
-      <div v-for="category in categories" :key="category">
+    <div class="grid grid-cols-3 gap-4 my-2">
+      <!-- <div v-for="category in categories" :key="category">
         <div
           class="
             w-full
@@ -26,43 +24,50 @@
               <h2 class="text-2xl text-white font-semibold">
                 {{ category.category_name }}
               </h2>
-              <p class="mt-2 text-gray-400">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Tempore facere provident molestias ipsam sint voluptatum
-                pariatur.
-              </p>
-              <button
-                class="
-                  flex
-                  items-center
-                  mt-4
-                  text-white text-sm
-                  uppercase
-                  font-medium
-                  rounded
-                  hover:underline
-                  focus:outline-none
-                "
-              >
-                <span>Shop Now</span>
-                <svg
-                  class="h-5 w-5 mx-2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-                </svg>
-              </button>
             </div>
           </div>
         </div>
+      </div> -->
+      <!--  -->
+
+      <div
+        class="relative overflow-hidden rounded"
+        style="height: 256px"
+        v-for="category in categories"
+        :key="category"
+      >
+        <p>
+          <a
+            class="st-link"
+            href="https://tourexpect.com/tour-packages/?location_id=10987"
+            ><br />
+            <img
+              class="w-full rounded block max-w-full brightness-75"
+              :src="
+                'http://127.0.0.1:8000/images/categories_images/' +
+                category.category_image
+              "
+            />
+          </a>
+        </p>
+        <div class="h-full">
+          <div
+            class="absolute top-1/2 left-1/2 text-center mt-4"
+            style="transform: translate(-50%, -50%)"
+          >
+            <a
+              href="https://tourexpect.com/tour-packages/?location_id=10987"
+              class="text-xl font-bold text-white"
+            >
+              {{ category.category_name }}
+            </a>
+          </div>
+        </div>
       </div>
+      <!--  -->
     </div>
   </div>
+  {{ categories }}
 </template>
 <script setup>
 const response = await $fetch("http://127.0.0.1:8000/api/all-categories");
